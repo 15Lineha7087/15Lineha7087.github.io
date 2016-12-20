686cca4748c67c9434b1d638af7ff125a37fc257
a15lineha7087 <rlinehan1962@gmail.com>
2016-12-20 15:58:29 +0000

    finalmusic

---
+++ finalmusic/songs.js
@@ -0,0 +1,19 @@
+/*
+	Each song is an .mp3
+	Each picture is a .png
+*/
+var songs = {
+	'1':{
+		song:"Frank Sinatra - Have Yourself A Merry Little Christmas",
+		picture: "MerryChristmas"
+	},
+	'2':{
+		song:"Brenda Lee - Rockin Around The Christmas Tree",
+		picture: "ChristmasTree"
+	},
+	'3':{
+		song:"Nat King Cole - The Christmas Song (Merry Christmas To You)",
+		picture: "ChristmasSong"
+	}
+};
+
